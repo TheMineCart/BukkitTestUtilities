dbedaa5e385afdf19810a63f5957ba7967499108
Jason Berry <jberry@cyrusinnovation.com>
2012-05-08 16:02:26 -0400

    Made into a Maven project and added a way to freeze time to a specific time.  Version 0.1.6

--- src/main/java/tmc/BukkitTestUtilities/Services/TimeFreezeService.java
+++ src/main/java/tmc/BukkitTestUtilities/Services/TimeFreezeService.java
@@ -1,8 +1,14 @@
 package tmc.BukkitTestUtilities.Services;
 
+import org.joda.time.DateTime;
 import org.joda.time.DateTimeUtils;
 
 public class TimeFreezeService {
+
+    public static void freeze(DateTime time) {
+        DateTimeUtils.setCurrentMillisFixed(time.getMillis());
+    }
+
     public static void freeze() {
         DateTimeUtils.setCurrentMillisFixed(DateTimeUtils.getInstantMillis(null));
     }
